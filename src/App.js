c2a7fff321ff009082385959df89902dbebf174c
krbrennan <krb.brennan@gmail.com>
2018-08-15 15:27:14 -0400

    Working DarkSky API call.

--- src/App.js
+++ src/App.js
@@ -9,38 +9,70 @@ class App extends Component {
     super(props);
     this.state = {
       data: [],
+      darkSkyData: [],
       searchField: '',
+      lat: 0,
+      long: 0,
       searched: false
     };
     this.handler = this.handler.bind(this)
-  }
-  getCoords(){
-    navigator.geolocation.getCurrentPosition(function(position) {
-      let lat = position.coords.latitude;
-      let long = position.coords.longitude;
-    });
+    // this.getCoords = this.getCoords.bind(this)
   }
 
+  // async function getForecast(lat,long) {
+  //   const proxy = `https://thingproxy.freeboard.io/fetch/`
+  //   const url = `https://api.darksky.net/forecast/3c36360a47f4cc747e19871230e1ecd8/${lat},${long}`
+  //   const response = await fetch(proxy + url)
+  //   hideLoader()
+  //   return parseResults(await response.json())
+  // }
 
 componentDidMount(){
+  // retreives latitude and longitude for darkSky API
+  navigator.geolocation.getCurrentPosition(position => {
+    let lat = Math.ceil(position.coords.latitude * 100) / 100;
+    let long = Math.ceil(position.coords.longitude * 100) / 100;
+    this.setState({
+      lat: lat,
+      long: long
+    })
+  });
 }
 
+async componentDidUpdate(){
 
-  // handler(data) {
-  //   console.log(data)
-  //   this.setState({
-  //     searchField: data,
-  //     searched: true
-  //   });
-  //   const zip = data
-  //   const url = `https://api.openweathermap.org/data/2.5/weather?q=${data},us&APPID=ac628c4c599f5392d529f22369c7f85f&units=imperial`
-  // }
+
+      // if(this.state.searched === true){
+      //   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.searchField},us&APPID=ac628c4c599f5392d529f22369c7f85f&units=imperial`);
+      //   const json = await response.json();
+      //   this.setState({
+      //     data: json,
+      //     lat: json.coord.lat,
+      //     long: json.coord.lon
+      //   });
+      // need to set searched to false to stop infinite API calls
+      //   this.setState({searched: false})
+      // }
+
+    if(this.state.lat !== 0 && searched == true){
+      const proxy = `https://cors-anywhere.herokuapp.com/`
+      const url = `https://api.darksky.net/forecast/3c36360a47f4cc747e19871230e1ecd8/${this.state.lat},${this.state.long}`
+      const darkResponse = await fetch(proxy + url)
+      const darkJson = await darkResponse.json()
+      this.setState({
+        darkSkyData: darkJson,
+        searched: false
+      });
+    }
+}
+
 
   handler(data){
     // successfully sets state.searchField to zip code
     this.setState({
-      searchField: data
-    })
+      searchField: data,
+      searched: true
+    });
 
   }
 
