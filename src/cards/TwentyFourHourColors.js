fe172388522090452b2899596636f94f0450ea0d
krbrennan <krb.brennan@gmail.com>
2019-09-13 13:36:56 -0400

    styled 24 hours for responsive viewing

--- src/cards/TwentyFourHourColors.js
+++ src/cards/TwentyFourHourColors.js
@@ -4,35 +4,46 @@ const TwentyFourHourColors = (props) => {
 
   let colors = []
   let numHours = 0
-
+console.log(props)
   if(props.props.props !== undefined){
     // console.log(props.props.data)
       props.props.props.map(hour => {
-        switch(hour.icon){
-          case "cloudy":
-          colors.push('#636e72')
-          ++numHours
-          break
-          case 'partly-cloudy-day':
-          colors.push('#b2bec3')
-          ++numHours
-          break
-          case 'rain':
-          colors.push('#0652DD')
-          ++numHours
-          break
-          case 'sunny':
-          colors.push('#FFC312')
-          ++numHours
-          break
+        switch(hour.summary){
+          case "Cloudy":
+            colors.push('#A9A9A9')
+            ++numHours
+            break
+          case 'Partly Cloudy':
+            colors.push('#D3D3D3')
+            ++numHours
+            break
+          case 'Rain':
+            colors.push('#0652DD')
+            ++numHours
+            break
+          case 'Sunny':
+            colors.push('#FFC312')
+            ++numHours
+            break
           case 'clear-night':
-          colors.push('#FFC312')
-          ++numHours
-          break
+            colors.push('#FFC312')
+            ++numHours
+            break
           case 'clear-day':
-          colors.push('#FFC312')
-          ++numHours
-          break
+            colors.push('#FFC312')
+            ++numHours
+            break
+          case 'Overcast':
+            colors.push('#DCDCDC')
+            ++numHours
+            break
+          case 'Mostly Cloudy':
+            colors.push('#C0C0C0')
+            ++numHours
+            break
+          default:
+            colors.push("#0000")
+            break
         }
       });
   }
@@ -42,7 +53,7 @@ const TwentyFourHourColors = (props) => {
   if(timeline !== undefined){
     return(
         colors.map((color) => {
-          return <span style={{backgroundColor: color, width: '66.4px'}}></span>
+          return <span style={{backgroundColor: color }}></span>
         })
     )
   } else{
